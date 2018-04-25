b25a86c472b00d77a16e94fe068fa2bf2fba75ba
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-04-25 19:37:51 +0200

    feat(layout): Use flex to distribute of the main layout

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="h-full flex flex-col">
     <header>
       <slot name="header"></slot>
     </header>
-    <main class="container mx-auto text-center mt-8">
-      <slot/>
+    <main class="flex-1">
+      <div class="container text-center mt-8">
+        <slot/>
+      </div>
     </main>
     <footer>
       <slot name="footer"></slot>
