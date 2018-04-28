85319338fc6cd477c40ce303ca1f860fa88c4a6d
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-04-28 19:38:07 +0200

    feat(header): Improve logo and text styles

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <img src="/static/img/favicon/favicon-64x64.png" alt="header-logo" />
+  <div class="bg-teal">
+    <div class="container py-2 flex">
+      <div class="w-10">
+        <img src="/static/img/favicon/favicon-64x64.png" alt="header-logo" />
+      </div>
+      <div class="flex-1 flex items-center justify-center text-white text-2xl">
+        PollMe
+      </div>
+    </div>
   </div>
 </template>
 
