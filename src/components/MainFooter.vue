7de84085825850eb668c9c52e5adc35b4dcfad5f
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-04-29 13:57:12 +0200

    feat(footer): Add text for community, project, license and contributing

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="bg-black">
-    Footer
+  <div class="bg-black text-white">
+    <div class="container py-3 text-xs text-center">
+      <p>
+        This is an
+        <a class="text-primary" target="_blank" href="https://github.com/Poll-me/poll-me-vue">
+          open source project
+        </a>
+        made with <font-awesome-icon icon="heart"></font-awesome-icon> by
+        <a class="text-primary" target="_blank" href="https://github.com/Poll-me">
+          PollMe community
+        </a>
+        under the
+        <a class="text-primary" target="_blank"
+          href="https://github.com/Poll-me/poll-me-vue/blob/master/LICENSE">
+          MIT license
+        </a>
+      </p>
+      <p class="mt-2">
+        You can contribute by coding or propose new features
+        <a class="text-primary" target="_blank"
+          href="https://github.com/Poll-me/poll-me-vue/issues">
+          here
+        </a>
+      </p>
+    </div>
   </div>
 </template>
 
