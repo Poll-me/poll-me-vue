3318b863508430d61878e428c6fce5e9b540c58a
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-04-30 13:45:13 +0200

    feat(polls): Create entities store node and items getter and use it at polls component

--- src/polls/UserPolls.vue
+++ src/polls/UserPolls.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>User polls!!! {{ pollMessage }}</div>
+  <div>
+    <div v-for="poll in items" :key="poll.name"
+      class="rounded overflow-hidden border-2 border-primary my-2">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ poll.name }}</div>
+        <p class="text-grey-darker text-base">
+          {{ poll.description }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { mapState } from 'vuex';
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapGetters } = createNamespacedHelpers('polls');
 
 @Component({
-  computed: mapState('polls', ['pollMessage'])
+  computed: mapGetters(['items'])
 })
 export default class UserPolls extends Vue {
 
