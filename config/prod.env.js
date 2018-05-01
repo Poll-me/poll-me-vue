08887cbc70974b52c0bc9bddb9b0444673d7a705
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-01 13:27:49 +0200

    chore(env): Stringify environment variables values for DefinePlugin

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FB_API_KEY: process.env.FB_API_KEY,
-  FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
-  FB_DB_URL: process.env.FB_DB_URL,
-  FB_PROJECT_ID: process.env.FB_PROJECT_ID,
-  FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
-  FB_MSG_SENDER_ID: process.env.FB_MSG_SENDER_ID
+  FB_API_KEY: JSON.stringify(process.env.FB_API_KEY),
+  FB_AUTH_DOMAIN: JSON.stringify(process.env.FB_AUTH_DOMAIN),
+  FB_DB_URL: JSON.stringify(process.env.FB_DB_URL),
+  FB_PROJECT_ID: JSON.stringify(process.env.FB_PROJECT_ID),
+  FB_STORAGE_BUCKET: JSON.stringify(process.env.FB_STORAGE_BUCKET),
+  FB_MSG_SENDER_ID: JSON.stringify(process.env.FB_MSG_SENDER_ID)
 }
