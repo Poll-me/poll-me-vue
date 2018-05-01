dae6eb5a87c801e40dcecc40d31433e6acde7b7f
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-01 13:19:22 +0200

    chore(firebase): Define the config through environment variables

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FB_API_KEY: process.env.FB_API_KEY,
+  FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+  FB_DB_URL: process.env.FB_DB_URL,
+  FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+  FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+  FB_MSG_SENDER_ID: process.env.FB_MSG_SENDER_ID
 }
