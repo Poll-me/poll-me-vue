2733410f3a103c977d6ea213003fca51f35ca6d5
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-03 13:12:15 +0200

    refactor(polls): Rename the poll exists guard

--- src/polls/router/index.js
+++ src/polls/router/index.js
@@ -1,7 +1,7 @@
 import Router from 'vue-router';
 
 import UserPolls from '../UserPolls';
-import pollGuard from './poll-guard';
+import pollExistsGuard from './poll-exists.guard';
 
 export default new Router({
   base: '/polls',
@@ -16,7 +16,7 @@ export default new Router({
       path: '/:key',
       name: 'poll',
       component: () => import('../poll'),
-      beforeEnter: pollGuard
+      beforeEnter: pollExistsGuard
     }
   ]
 });
