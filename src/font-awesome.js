e91c4fea23daca588b54c6edeb7ea0f3d8c3baed
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-04 21:19:34 +0200

    feat(icons): Add brand fontawesome icons package and import the whatsapp icon

--- src/font-awesome.js
+++ src/font-awesome.js
@@ -1,8 +1,9 @@
 import fontawesome from '@fortawesome/fontawesome';
+import { faWhatsapp } from '@fortawesome/fontawesome-free-brands';
 import solid from '@fortawesome/fontawesome-free-solid';
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
 import Vue from 'vue';
 
-fontawesome.library.add(solid);
+fontawesome.library.add(solid, faWhatsapp);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
