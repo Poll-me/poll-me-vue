78d4ff1ab92aec04816e337d125da87c72bb1951
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-04 17:28:35 +0200

    feat(poll registration): Add a form to register a new name in the poll

--- src/polls/poll/fill/components/RegistrationPoll.vue
+++ src/polls/poll/fill/components/RegistrationPoll.vue
@@ -2,10 +2,21 @@
   <div class="">
     <div class="text-center pb-2">People IN</div>
     <div >
-      <ul class="list-reset flex flex-wrap -m-1 text-sm text-center">
+      <form @submit.prevent="submit"
+        class="bg-blue-dark rounded p-2 mb-2 flex">
+        <label for="name"
+          class="text-white font-semibold flex-no-shrink pl-2 pr-4 flex items-center">
+          Are you in?
+        </label>
+        <div class="flex-1" >
+          <input id="name" type="text" placeholder="Your name" v-model.trim="name"
+            class="shadow appearance-none rounded w-full p-2 text-grey-darker text-sm">
+        </div>
+      </form>
+      <ul class="list-reset flex flex-wrap -m-1 text-sm text-center text-white">
         <li v-for="ans in poll.answers" :key="ans.author"
           class="w-1/2 p-1" >
-          <div class="border p-2 rounded h-full flex flex-col justify-center">
+          <div class="bg-teal-dark shadow p-2 rounded h-full flex flex-col justify-center">
             {{ ans.author }}
           </div>
         </li>
@@ -24,11 +35,14 @@ import Component from 'vue-class-component';
       type: Object,
       validator: poll => poll.type === 1
     },
-    answers: Array,
     config: Object
   }
 })
 export default class RegistrationPoll extends Vue {
+  name = '';
 
+  submit() {
+    this.$emit('vote', this.name);
+  }
 }
 </script>
