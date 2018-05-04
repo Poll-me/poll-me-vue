0a36977bfc47acc3ee39ea3648f14f000fc98a87
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-04 13:13:59 +0200

    feat(poll): Create action and mutations to fetch poll answers

--- src/polls/poll/store/actions/index.js
+++ src/polls/poll/store/actions/index.js
@@ -5,12 +5,35 @@ const db = fb.database();
 export default {
   fetchPoll({ commit, state }, { key }) {
     if (state.key) {
-      db.ref('/polls').child(state.key).off();
+      db
+        .ref('/polls')
+        .child(state.key)
+        .off();
     }
     commit('setKey', { key });
 
-    db.ref('/polls').child(key).on('value', (snapshot) => {
-      commit('setEntity', { poll: snapshot.val() });
-    });
+    db
+      .ref('/polls')
+      .child(key)
+      .on('value', snapshot => commit('setEntity', { poll: snapshot.val() }));
+  },
+
+  fetchAnswers({ commit, state }, { key }) {
+    if (state.key) {
+      db
+        .ref('/answers')
+        .orderByChild('poll')
+        .equalTo(state.key)
+        .off();
+    }
+
+    db
+      .ref('/answers')
+      .orderByChild('poll')
+      .equalTo(key)
+      .on('value', (snapshot) => {
+        const answers = snapshot.val() || {};
+        commit('setAnswers', { answers });
+      });
   }
 };
