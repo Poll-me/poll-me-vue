897d99ccd207e3b619a806d51de70d1c856bc394
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-06 21:45:46 +0200

    feat(polls new): Add a description field and a create button

--- src/polls/create/CreatePoll.vue
+++ src/polls/create/CreatePoll.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col">
     <div class="bg-primary text-white py-4">
       <div class="container">
-        <h1 class="text-lg font-medium text-center">Create a poll</h1>
+        <h1 class="text-lg font-medium text-center">New poll</h1>
         <select v-show="!type" v-model="type"
           id="poll-type"
           class="mt-4">
@@ -13,7 +13,7 @@
         </select>
       </div>
     </div>
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
       <template v-if="selectedType">
         <div class="bg-blue-dark text-white" >
           <div class="container py-4">
@@ -25,24 +25,38 @@
             <small class="italic">{{ selectedType.description }}</small>
           </div>
         </div>
-        <form class="container py-4">
+        <form class="flex-1 container py-4">
           <div class="flex items-center mb-4">
             <label for="poll-name">
               Name:
             </label>
-            <input v-model="name"
+            <input v-model.trim="name"
               id="poll-name" type="text" placeholder="The poll topic">
           </div>
           <div class="flex items-center">
             <label for="poll-author">
               Author:
             </label>
-            <input v-model="author"
+            <input v-model.trim="author"
               id="poll-author" type="text" placeholder="Who is asking?">
           </div>
+          <div class="">
+            <label for="poll-description" class="mt-4 mb-2">
+              Description:
+            </label>
+            <textarea v-model.trim="description"
+              id="poll-author" rows="4" placeholder="Tell about what to participate in">
+            </textarea>
+          </div>
         </form>
+        <div class="sticky pin-b py-4 bg-secondary">
+          <div class="text-center text-white text-xl">
+            Create
+            <font-awesome-icon icon="plus" class="ml-1" ></font-awesome-icon>
+          </div>
+        </div>
       </template>
-      <div v-else class="container h-full flex items-center text-center" >
+      <div v-else class="container flex-1 flex items-center text-center" >
         <div class="flex-1">
           <font-awesome-icon icon="arrow-up" size="2x"></font-awesome-icon>
           <div class="mt-8">First, select what kind of poll you want to create</div>
@@ -64,6 +78,7 @@ const { mapState } = createNamespacedHelpers('polls');
 export default class CreatePoll extends Vue {
   name = '';
   author = '';
+  description = '';
   type = 0;
 
   get selectedType() {
