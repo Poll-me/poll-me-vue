d391309a793afea141df5337bf2ee91b69ec73fd
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-08 12:27:45 +0200

    feat(header): Use the title font on the header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bg-secondary shadow">
-    <div class="container py-2 flex">
+    <div class="container py-2 flex text-white">
       <div class="w-10">
         <img src="/static/img/favicon/favicon-64x64.png" alt="header-logo" />
       </div>
-      <div class="flex-1 flex items-center justify-center text-white text-xl font-medium">
+      <div class="flex-1 flex items-center justify-center text-2xl font-medium font-title">
         PollMe
       </div>
-      <div class="w-10 flex items-center justify-center text-white text-3xl">
+      <div class="flex items-center justify-center text-3xl">
         <font-awesome-icon :icon="['fab', 'pied-piper-alt']" fixed-width>
         </font-awesome-icon>
       </div>
