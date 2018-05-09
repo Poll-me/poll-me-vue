9128fb27db86750c4940c47673f5d5b3f4cc19b5
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-09 14:33:46 +0200

    feat(404): Create a funny NotFoundPage component

---
+++ src/NotFoundPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container py-4 text-center">
+    <div class="font-title">
+      <h1>PAGE NOT FOUND</h1>
+      <img src="/static/img/vader_computer.jpg">
+      <h2 class="">A wild 404 error appeared!!</h2>
+      <div class="mt-4">But keep calm, you still can go to:</div>
+    </div>
+    <div class="flex mt-2">
+      <router-link class="flex-1 bg-primary rounded text-white p-2" to="/">
+        Home page
+      </router-link>
+      <router-link class="flex-1 bg-primary rounded text-white p-2 ml-2" :to="{ name: 'new-poll' }">
+        New poll
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component()
+export default class NotFoundPage extends Vue {
+}
+</script>
