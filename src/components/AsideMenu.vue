d7bb84c294fab0aa244a588ca65ed9c231ef8bf5
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-09 20:22:43 +0200

    feat(header): Create a nav-list css component and apply to the aside nav

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="absolute z-50 pin-x bg-secondary shadow-md">
-    <nav class="container pb-2 text-center">
-      <ul class="list-reset">
-        <li class="py-2">
+    <nav class="container pb-2 text-center nav-list">
+      <ul>
+        <li>
           <router-link to="/">
             Home
           </router-link>
         </li>
-        <li class="py-2">
+        <li>
           <router-link :to="{ name: 'user-polls' }">
             Your polls
           </router-link>
         </li>
-        <li class="py-2">
+        <li>
           <router-link :to="{ name: 'new-poll' }">
             New poll
           </router-link>
         </li>
-        <li class="py-2">
+        <li>
           <router-link :to="{ name: 'profile' }">
             Profile
           </router-link>
