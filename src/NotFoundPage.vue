04ad2c40332c02de280a1ec97b7085b4ea2d8ecd
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-09 15:08:19 +0200

    feat(404): Add translations for the content

--- src/NotFoundPage.vue
+++ src/NotFoundPage.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container py-4 text-center">
     <div class="font-title">
-      <h1>PAGE NOT FOUND</h1>
-      <img src="/static/img/vader_computer.jpg">
-      <h2 class="">A wild 404 error appeared!!</h2>
-      <div class="mt-4">But keep calm, you still can go to:</div>
+      <h1 class="uppercase" v-t="'not-found.title'"></h1>
+      <img class="inline-block" alt="vader" src="/static/img/vader_computer.jpg">
+      <h2 v-t="'not-found.description'"></h2>
+      <div class="mt-4" v-t="'not-found.info'"></div>
     </div>
     <div class="flex mt-2">
       <router-link class="flex-1 btn btn-primary" to="/">
         <font-awesome-icon icon="home"></font-awesome-icon>
-        Home page
+        <span v-t="'not-found.home-link'"></span>
       </router-link>
       <router-link class="flex-1 btn btn-primary ml-2" :to="{ name: 'new-poll' }">
         <font-awesome-icon icon="plus"></font-awesome-icon>
-        New poll
+        <span v-t="'not-found.create-link'"></span>
       </router-link>
     </div>
   </div>
