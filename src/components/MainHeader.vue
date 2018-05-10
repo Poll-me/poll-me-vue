b2c9a772ec39e06ce598eda367b879d9fe786360
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-10 10:12:59 +0200

    feat(header): Add a link to the home at the logo and title

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,14 +2,15 @@
   <div class="bg-secondary" :class="{ shadow: !navMenuOpen }">
     <div class="container py-2 flex text-white bg-secondary relative z-20">
       <div class="w-10">
-        <img src="/static/img/favicon/favicon-64x64.png" alt="header-logo" />
+        <router-link to="/" class="text-white">
+          <img src="/static/img/favicon/favicon-64x64.png" alt="header-logo" />
+        </router-link>
       </div>
       <div class="flex-1 flex items-center justify-center text-2xl font-medium font-title">
-        PollMe
+        <router-link to="/" class="text-white">PollMe</router-link>
       </div>
-      <div class="flex items-center justify-center text-3xl">
-        <font-awesome-icon :icon="menuIcon" fixed-width
-          @click="navMenuOpen = !navMenuOpen"
+      <div class="flex items-center justify-center text-3xl w-10">
+        <font-awesome-icon :icon="menuIcon" @click="navMenuOpen = !navMenuOpen"
           class="cursor-pointer">
         </font-awesome-icon>
       </div>
