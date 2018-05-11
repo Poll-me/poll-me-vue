0a4c1de995e8c442839e77d7ef6e9bc6e6052023
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-11 14:07:44 +0200

    refactor(user login): Move the firebaseui init to a specific method

--- src/user/login/FirebaseUIForm.vue
+++ src/user/login/FirebaseUIForm.vue
@@ -5,22 +5,30 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import firebase from 'firebase';
-import * as firebaseui from 'firebaseui';
+import firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 
 @Component()
 export default class FirebaseUIForm extends Vue {
+  ui;
+
   mounted() {
-    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-container', {
-      signInOptions: [
-        {
-          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          requireDisplayName: false
-        },
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      ]
-    });
+    this.initFirebaseUI();
+  }
+
+  initFirebaseUI() {
+    if (!this.ui) {
+      this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+      this.ui.start('#firebaseui-container', {
+        signInOptions: [
+          {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: true
+          },
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ]
+      });
+    }
   }
 }
 </script>
