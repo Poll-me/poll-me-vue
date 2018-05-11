3f78cdc3962069a4352d6747bc7fa35bb68898af
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-11 11:05:04 +0200

    feat(user login): Add title and descrition

--- src/user/login/LoginPage.vue
+++ src/user/login/LoginPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Hello login!!</div>
+  <div>
+    <div class="bg-primary text-white">
+      <div class="container py-4 text-center">
+        <h1 class="text-xl" v-t="'user.login.title'"></h1>
+        <p class="mt-2 text-xs italic" v-t="'user.login.description'"></p>
+      </div>
+    </div>
+    <div class="container">
+    </div>
+  </div>
 </template>
 <script>
 import Vue from 'vue';
