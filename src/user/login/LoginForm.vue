3e1d57fc8b6e04659ae32487021a5dff1843d8d2
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-13 02:22:55 +0200

    feat(user login): Add translations to the form

--- src/user/login/LoginForm.vue
+++ src/user/login/LoginForm.vue
@@ -2,28 +2,24 @@
   <form class="flex flex-col" @submit.prevent="submit">
     <div class="flex-1 container py-4">
       <div class="mb-4">
-        <label class="mb-2" for="email">
-          Email
-        </label>
+        <label class="mb-2" for="email" v-t="'user.login.email.label'"></label>
         <input v-model.trim="email" @input="$v.email.$touch()"
-          :class="{ 'border-red': $v.email.$error }"
-          id="email" name="email" type="email" placeholder="Username" required >
+          :class="{ 'border-red': $v.email.$error }" required
+          id="email" type="email" :placeholder="$t('user.login.email.placeholder')" >
         <p v-show="$v.email.$error" class="text-red text-xs italic mt-3">
-          <span v-show="!$v.email.required">Email is required.</span>
-          <span v-show="!$v.email.email">Write a valid email direction.</span>
+          <span v-show="!$v.email.required" v-t="'user.login.email.required-error'"></span>
+          <span v-show="!$v.email.email" v-t="'user.login.email.valid-error'"></span>
         </p>
       </div>
       <div class="mb-4">
-        <label class="mb-2" for="password">
-          Password
-        </label>
+        <label class="mb-2" for="password" v-t="'user.login.password.label'"></label>
         <input v-model.trim="password" @input="$v.password.$touch()"
           :class="{ 'border-red': $v.password.$error }"
           id="password" type="password" placeholder="********" required >
         <p v-show="$v.password.$error" class="text-red text-xs italic mt-3">
-          <span v-show="!$v.password.required">Please choose a password.</span>
-          <span v-show="!$v.password.minLength">
-            It have to be {{ $v.password.$params.minLength.min }} length.
+          <span v-show="!$v.password.required" v-t="'user.login.password.required-error'"></span>
+          <span v-show="!$v.password.minLength"
+            v-t="{ path: 'user.login.password.min-length-error', args: { length: $v.password.$params.minLength.min } }">
           </span>
         </p>
       </div>
@@ -31,7 +27,7 @@
     <button :class="{ 'opacity-75': $v.$invalid }" :disabled="$v.$invalid"
       type="submit" class="sticky pin-b py-4 bg-secondary text-center text-white text-xl">
       <font-awesome-icon icon="sign-in-alt" class="mr-1" ></font-awesome-icon>
-      <span >Sign in</span>
+      <span v-t="'user.login.submit'"></span>
     </button>
   </form>
 </template>
