679ad8a9dd04a81a39df4f1a4cf2eceaf69352a2
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-16 12:46:24 +0200

    chore(firebase): Add fallback to firebase prod env config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,11 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  FB_API_KEY: '"AIzaSyAncYW-RYkegv0gq8_laAlUTe3vfBd6KEY"',
-  FB_AUTH_DOMAIN: '"poll-me-36f9a.firebaseapp.com"',
-  FB_DB_URL: '"https://poll-me-36f9a.firebaseio.com"',
-  FB_PROJECT_ID: '"poll-me-36f9a"',
-  FB_STORAGE_BUCKET: '"poll-me-36f9a.appspot.com"',
-  FB_MSG_SENDER_ID: '"772514369454"'
+  NODE_ENV: '"development"'
 })
