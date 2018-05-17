321beb7e8515de647cb54932e0b654cb37f8f8a9
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-05-17 21:11:42 +0200

    feat(user profile): Add controls for update the password

--- src/user/profile/UserProfile.vue
+++ src/user/profile/UserProfile.vue
@@ -31,6 +31,39 @@
           <label class="mb-2" v-t="'user.login.email.label'"></label>
           <input id="email" type="email" :value="profile.email" disabled >
         </div>
+        <div class="mb-4">
+          <label class="mb-2 flex pr-0 items-center">
+            <div class="flex-1" v-t="'user.profile.change-password'"></div>
+            <switches id="change-password" v-model="changePassword" color="blue" type-bold="true" >
+            </switches>
+          </label>
+          <div v-if="changePassword">
+            <div class="mb-4">
+              <label class="mb-2" for="currentPassword"
+                v-t="'user.profile.current-password.label'"></label>
+              <input v-model.trim="currentPassword" @input="$v.currentPassword.$touch()"
+                id="currentPassword" type="password" required
+                :class="{ 'border-red': $v.currentPassword.$error }"
+                :placeholder="$t('user.profile.current-password.placeholder')" >
+            </div>
+            <div class="mb-4">
+              <label class="mb-2" for="newPassword"
+                v-t="'user.profile.new-password.label'"></label>
+              <input v-model.trim="newPassword" @input="$v.newPassword.$touch()"
+                id="newPassword" type="password" required
+                :class="{ 'border-red': $v.newPassword.$error }"
+                :placeholder="$t('user.profile.new-password.placeholder')" >
+            </div>
+            <div class="mb-4">
+              <label class="mb-2" for="confirmPassword"
+                v-t="'user.profile.confirm-password.label'"></label>
+              <input v-model.trim="confirmPassword" @input="$v.confirmPassword.$touch()"
+                id="confirmPassword" type="password" required
+                :class="{ 'border-red': $v.confirmPassword.$error }"
+                :placeholder="$t('user.profile.confirm-password.placeholder')" >
+            </div>
+          </div>
+        </div>
       </div>
       <button v-show="hasChanges" :class="{ 'opacity-75': $v.$invalid }" :disabled="$v.$invalid"
         type="submit" class="sticky pin-b py-4 bg-secondary text-center text-white text-xl">
@@ -43,12 +76,14 @@
 <script>
 import Component from 'vue-class-component';
 import { mapState, mapActions } from 'vuex';
-import { required, maxLength, minLength } from 'vuelidate/lib/validators';
+import { required, requiredIf, sameAs, maxLength, minLength } from 'vuelidate/lib/validators';
+import Switches from 'vue-switches';
 
 import { VueWithValidations } from '@/utils';
 
 
 @Component({
+  components: { Switches },
   computed: mapState({
     profile: state => state.user.profile
   }),
@@ -58,6 +93,19 @@ import { VueWithValidations } from '@/utils';
       required,
       minLength: minLength(6),
       maxLength: maxLength(24)
+    },
+    currentPassword: {
+      requiredIf: requiredIf('changePassword'),
+      minLength: minLength(8)
+    },
+    newPassword: {
+      requiredIf: requiredIf('changePassword'),
+      minLength: minLength(8),
+      notSameAsCurrentPassword(val) { return val !== this.currentPassword }
+    },
+    confirmPassword: {
+      requiredIf: requiredIf('changePassword'),
+      sameAsNewPassword: sameAs('newPassword')
     }
   }
 })
@@ -66,14 +114,14 @@ export default class UserProfile extends VueWithValidations {
   photoUrl = '';
   changePassword = false;
   currentPassword = '';
-  newPasswrod = '';
+  newPassword = '';
   confirmPassword = '';
 
   get hasChanges() {
     const { displayName, photoUrl } = this.profile;
     return this.name !== displayName ||
       (this.photoUrl.length > 0 && this.photoUrl !== photoUrl) ||
-      this.newPasswrod.length > 0;
+      this.newPassword.length > 0;
   }
 
   get updateData() {
