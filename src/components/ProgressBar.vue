5c341a344500bafdde2c4f27c0f62c84dac7b75c
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2018-06-14 20:48:57 +0200

    feat(components): Create a progress bar component to show percent results

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="rounded-lg">
+    <div class="rounded-lg text-white h-8 flex items-center"
+      :class="`bg-${color}`"
+      :style="{ width: `${roundedValue}%` }" >
+      <div class="text-center truncate px-2 flex-1">{{ visibleValue }}</div>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  props: {
+    value: {
+      type: Number,
+      required: true,
+      validator(val) { return val >= 0 && val <= 100; }
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  }
+})
+export default class ProgressBar extends Vue {
+  maxVisibleValue = 20;
+
+  get roundedValue() {
+    const limitedValue = Math.min(Math.max(this.value, 0), 100);
+    return Math.round(limitedValue);
+  }
+
+  get visibleValue() {
+    return this.roundedValue >= this.maxVisibleValue ? `${this.roundedValue} %` : '';
+  }
+}
+</script>
